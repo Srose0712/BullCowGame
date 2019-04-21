6084554f2004bb11c147ea1f7127ec7743ff81e0
SRose07 <SRose07@github.com>
2019-04-21 15:25:35 -0500

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Samuel Rose on 4/21/19.
+//  Copyright © 2019 Samuel Rose. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
